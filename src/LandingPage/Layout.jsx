346ca1d3ddd8694f23b4e8e85a39733cc8526abb
Ismael <ismael.lajud65@gmail.com>
2024-10-18 02:53:10 +0200

    actualizando

--- src/LandingPage/Layout.jsx
+++ src/LandingPage/Layout.jsx
@@ -1,20 +1,44 @@
-
+import { useEffect } from 'react'
+import Lenis from 'lenis'
 import HeroPage from '../components/Hero/HeroPage'
 import Perfomance from '../components/PerfomanceSection/Perfomance'
 
-
+import 'lenis/dist/lenis.css'
 import Seccion2 from '../components/whoweareSection/Seccion2'
+
 
 
 const Layout = () => {
 
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true, // Activa el smooth scroll
+      duration: 1.5, // Opcional: Ajusta la duración del scroll suave
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Función de easing personalizada
+      direction: 'vertical', // Scroll en vertical
+      gestureDirection: 'vertical', // Reconocer gestos de scroll vertical
+      smoothTouch: true, // Suaviza el scroll en dispositivos táctiles
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    // Cleanup para cuando el componente se desmonte
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
   return (
     <>
-   
+
     <HeroPage/>
     <Seccion2/>
     <Perfomance/>
-
+   
  
     </>
   )
